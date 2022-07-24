41c9242e2689108c04a855a058d941c671683bbd
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-07-25 03:54:13 +0500

    bcrypt hashing and salt-added

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
@@ -26,12 +27,15 @@ router.post(
       if (check) {
         return res.status(400).json({ error: "Error! Email exist already" });
       }
+      //Adding Salt to password and hashing it using bcyrptJS
+      const saltPass = await bcrypt.genSalt(10);
+      const securedPass = await bcrypt.hash(req.body.password, saltPass);
 
       //Creates a new user
       let user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securedPass,
       });
       res.json(user);
     } catch (err) {

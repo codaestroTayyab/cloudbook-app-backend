9e8a46d3e2eadd38c96d66d8a332fa641e56ac80
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-07-25 21:51:22 +0500

    Login endpoint- added

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 
 const router = express.Router();
-const AUTH_SECRET = "$Cloudbook$Mern"
+const AUTH_SECRET = "$Cloudbook$Mern";
 
 // POST METHOD for /cloudbook/auth/createuser. No login required
 router.post(
@@ -42,20 +42,64 @@ router.post(
 
       //Making JWT Token
       const tokenData = {
-          email: user.email
-      }
+        email: user.email,
+      };
       let authToken = jwt.sign(tokenData, AUTH_SECRET);
       //Sending JWT Token to user to authenticate
-      res.json({authToken});
+      res.json({ authToken });
 
       // res.json(user);
     } catch (err) {
       res
         .status(500)
-        .json({ error: "Some error occured", message: err.message });
+        .json({ error: "Internal Server Error", message: err.message });
       console.error(err.message);
     }
   }
 );
 
+// POST METHOD for /cloudbook/auth/login. No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a Valid Email").isEmail(),
+    body("password", "Password cannot be empty").exists(),
+  ],
+  async (req, res) => {
+    //Check Validations and show errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      //Check Email exists in dataBase
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "Incorrect Email or Password" });
+      }
+      //Converts pass to hash and compare hash to Check the correct password
+      let passCheck = await bcrypt.compare(password, user.password);
+      if (!passCheck) {
+        return res.status(400).json({ error: "Incorrect Email or Password" });
+      }
+      //If both credentials are correct then sending the payload(tokenData)
+
+      //Making JWT Token
+      const tokenData = {
+        email: user.email,
+      };
+      let authToken = jwt.sign(tokenData, AUTH_SECRET);
+
+      //Sending JWT Token to user to authenticate
+      res.json({ authToken });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Internal Server Error", message: err.message });
+      console.error(err.message);
+    }
+  }
+);
 module.exports = router;

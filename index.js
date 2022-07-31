67b9f07aa706b16f1ee67d1a28a27b1f170e8ca7
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-08-01 03:18:04 +0500

    cors package - added

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const connectToMongoDB = require('./mongodb');
 const express = require('express')
+var cors = require('cors')
 connectToMongoDB();
+
 
 
 const app = express();
 const port = 5000;
 
+app.use(cors())
 app.use(express.json());
 
 app.use('/cloudbook/auth', require('./routes/auth'));

855fbee8c66f4890cc5233914a64e819d4a0e8ac
codaestroTayyab <tayyab.saeed1@gmail.com>
2022-08-30 01:42:54 +0500

    final commits

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/cloudbook/notes', require('./routes/notes'));
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static(path.join(__dirname, '/client/build')))
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
 }
 else{
